2fd9287b083d509181b2ba29e97d8fb13d060c84
DIGIART <ashwishah1808@gmail.com>
2024-07-29 14:50:13 +0530

    KHUSHI

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -125,7 +125,7 @@ const DetailPage = () => {
 
   return (
     <div className="pt-[125px] px-4">
-      <div className="max-w-4xl mx-auto p-4 border border-gray-300 rounded-lg shadow-lg bg-[#596E79]">
+      <div className="max-w-4xl mx-auto p-4 border border-gray-300 rounded-lg shadow-lg bg-[#596E79] mb-7">
         <div className="relative mb-4">
           <h1 className="text-2xl font-bold text-[#F0ECE4]">₹{property.price} /month</h1>
           <h2 className="text-xl font-semibold text-[#F0ECE4] absolute top-0 right-0">{property.name}</h2>
