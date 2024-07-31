117788b967296dcb98c3c7b1d47c01d4bccb4704
DIGIART <ashwishah1808@gmail.com>
2024-07-31 11:15:58 +0530

    signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -147,7 +147,7 @@ const Signup = ({ setIsAuthenticated, setName }) => {
     }
 
     try {
-      const response = await fetch('https://room-rooster-kappa.vercel.app/api/register', {
+      const response = await fetch('https://room-rooster.vercel.app/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
