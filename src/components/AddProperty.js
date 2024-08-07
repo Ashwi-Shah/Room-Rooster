bb1653bd4c3edcc3049d33905052664801ffef71
DIGIART <ashwishah1808@gmail.com>
2024-08-07 11:15:58 +0530

    commit

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,3 +1,286 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Resizer from "react-image-file-resizer";
+
+// const AddProperty = ({ onAddProperty }) => {
+//   const [property, setProperty] = useState({
+//     name: "",
+//     price: "",
+//     phoneNumber: "",
+//     location:"",
+//     description: "",
+//     images: [],
+//     sqft: "",
+//     bed: "",
+//     bath: "",
+//     ownername: "",
+//     FurnishedStatus: "",
+//     Perferredfor: "",
+//     ageofconstruction: "",
+//     info: "",
+//     Availability: "",
+//     deposit: ""
+//   });
+
+//   const [error, setError] = useState("");
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setProperty({
+//       ...property,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleImageChange = (e) => {
+//     const files = Array.from(e.target.files);
+//     if (files.length > 3) {
+//       alert("You can upload up to 3 images.");
+//       return;
+//     }
+//     const resizedImages = [];
+
+//     files.forEach(file => {
+//       Resizer.imageFileResizer(
+//         file,
+//         800,
+//         550,
+//         "JPEG",
+//         100,
+//         0,
+//         (uri) => {
+//           resizedImages.push(uri);
+//           if (resizedImages.length === files.length) {
+//             setProperty({
+//               ...property,
+//               images: resizedImages
+//             });
+//           }
+//         },
+//         "blob"
+//       );
+//     });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+  
+//     const formData = new FormData();
+//     Object.keys(property).forEach(key => {
+//       if (key === 'images') {
+//         property.images.forEach((image, index) => {
+//           formData.append(`image${index + 1}`, image);
+//         });
+//       } else {
+//         formData.append(key, property[key]);
+//       }
+//     });
+  
+//     console.log('FormData:', formData); // Add this line to check the data
+  
+//     try {
+//       const response = await fetch("https://room-rooster.vercel.app/details", {
+//         method: "POST",
+//         body: formData,
+//       });
+  
+//       if (response.ok) {
+//         const newProperty = await response.json();
+//         console.log("Property added successfully:", newProperty);
+//         if (onAddProperty) {
+//           onAddProperty(newProperty);
+//         }
+//         navigate("/"); // Redirect to home page after adding property
+//       } else {
+//         const errorData = await response.json();
+//         setError(`Error: ${response.statusText} - ${errorData.message}`);
+//         console.error("Error adding property:", errorData);
+//       }
+//     } catch (error) {
+//       setError("An unexpected error occurred. Please try again later.");
+//       console.error("Error adding property:", error);
+//     }
+//   };
+  
+//   return (
+//     <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-10 mb-10 transition-shadow duration-300 hover:shadow-2xl">
+//       <h2 className="text-3xl font-bold text-gray-700 mb-6 text-center">Add Property</h2>
+//       <form onSubmit={handleSubmit} className="space-y-4">
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Name:</label>
+//           <input
+//             type="text"
+//             name="name"
+//             value={property.name}
+//             onChange={handleChange}
+//             required
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Price (per month):</label>
+//           <input
+//             type="number"
+//             name="price"
+//             value={property.price}
+//             onChange={handleChange}
+//             required
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Phone Number:</label>
+//           <input
+//             type="tel"
+//             name="phoneNumber"
+//             value={property.phoneNumber}
+//             onChange={handleChange}
+//             required
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Location:</label>
+//           <input
+//             type="text"
+//             name="location"
+//             value={property.location}
+//             onChange={handleChange}
+//             required
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Address:</label>
+//           <textarea
+//             name="description"
+//             value={property.description}
+//             onChange={handleChange}
+//             required
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600 resize-y"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Property Image:</label>
+//           <input
+//             type="file"
+//             name="image"
+//             multiple
+//             onChange={handleImageChange}
+//             required
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Size (Sqft):</label>
+//           <input
+//             type="number"
+//             name="sqft"
+//             value={property.sqft}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Bedrooms:</label>
+//           <input
+//             type="number"
+//             name="bed"
+//             value={property.bed}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Bathrooms:</label>
+//           <input
+//             type="number"
+//             name="bath"
+//             value={property.bath}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Owner Name:</label>
+//           <input
+//             type="text"
+//             name="ownername"
+//             value={property.ownername}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Furnished Status:</label>
+//           <input
+//             type="text"
+//             name="FurnishedStatus"
+//             value={property.FurnishedStatus}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Preferred For:</label>
+//           <input
+//             type="text"
+//             name="Perferredfor"
+//             value={property.Perferredfor}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Age of Construction:</label>
+//           <input
+//             type="number"
+//             name="ageofconstruction"
+//             value={property.ageofconstruction}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//         </div>
+//         <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Additional Info:</label>
+//           <input
+//             type="text"
+//             name="info"
+//             value={property.info}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//           </div>
+//           <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">Availability:</label>
+//           <input
+//             type="text"
+//             name="Availability"
+//             value={property.Availability}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//           </div>
+//           <div className="flex flex-col">
+//           <label className="font-semibold text-gray-600 mb-2">deposit:</label>
+//           <input
+//             type="number"
+//             name="deposit"
+//             value={property.deposit}
+//             onChange={handleChange}
+//             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+//           />
+//           </div>
+//         <button type="submit" className="mt-6 w-full py-3 bg-gray-700 text-white font-bold rounded-lg hover:bg-gray-800 transition-colors">
+//           Add Property
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AddProperty;
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Resizer from "react-image-file-resizer";
@@ -7,7 +290,7 @@ const AddProperty = ({ onAddProperty }) => {
     name: "",
     price: "",
     phoneNumber: "",
-    location:"",
+    location: "",
     description: "",
     images: [],
     sqft: "",
@@ -39,8 +322,8 @@ const AddProperty = ({ onAddProperty }) => {
       alert("You can upload up to 3 images.");
       return;
     }
-    const resizedImages = [];
 
+    const resizedImages = [];
     files.forEach(file => {
       Resizer.imageFileResizer(
         file,
@@ -52,10 +335,10 @@ const AddProperty = ({ onAddProperty }) => {
         (uri) => {
           resizedImages.push(uri);
           if (resizedImages.length === files.length) {
-            setProperty({
-              ...property,
+            setProperty(prevState => ({
+              ...prevState,
               images: resizedImages
-            });
+            }));
           }
         },
         "blob"
@@ -65,29 +348,26 @@ const AddProperty = ({ onAddProperty }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const formData = new FormData();
     Object.keys(property).forEach(key => {
       if (key === 'images') {
         property.images.forEach((image, index) => {
-          formData.append(`image${index + 1}`, image);
+          formData.append(`images`, image);
         });
       } else {
         formData.append(key, property[key]);
       }
     });
-  
-    console.log('FormData:', formData); // Add this line to check the data
-  
+
     try {
       const response = await fetch("https://room-rooster.vercel.app/details", {
         method: "POST",
         body: formData,
       });
-  
+
       if (response.ok) {
         const newProperty = await response.json();
-        console.log("Property added successfully:", newProperty);
         if (onAddProperty) {
           onAddProperty(newProperty);
         }
@@ -95,17 +375,16 @@ const AddProperty = ({ onAddProperty }) => {
       } else {
         const errorData = await response.json();
         setError(`Error: ${response.statusText} - ${errorData.message}`);
-        console.error("Error adding property:", errorData);
       }
     } catch (error) {
       setError("An unexpected error occurred. Please try again later.");
-      console.error("Error adding property:", error);
     }
   };
-  
+
   return (
     <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-10 mb-10 transition-shadow duration-300 hover:shadow-2xl">
       <h2 className="text-3xl font-bold text-gray-700 mb-6 text-center">Add Property</h2>
+      {error && <div className="text-red-600 mb-4">{error}</div>}
       <form onSubmit={handleSubmit} className="space-y-4">
         <div className="flex flex-col">
           <label className="font-semibold text-gray-600 mb-2">Name:</label>
@@ -251,8 +530,8 @@ const AddProperty = ({ onAddProperty }) => {
             onChange={handleChange}
             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
           />
-          </div>
-          <div className="flex flex-col">
+        </div>
+        <div className="flex flex-col">
           <label className="font-semibold text-gray-600 mb-2">Availability:</label>
           <input
             type="text"
@@ -261,9 +540,9 @@ const AddProperty = ({ onAddProperty }) => {
             onChange={handleChange}
             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
           />
-          </div>
-          <div className="flex flex-col">
-          <label className="font-semibold text-gray-600 mb-2">deposit:</label>
+        </div>
+        <div className="flex flex-col">
+          <label className="font-semibold text-gray-600 mb-2">Deposit:</label>
           <input
             type="number"
             name="deposit"
@@ -271,10 +550,15 @@ const AddProperty = ({ onAddProperty }) => {
             onChange={handleChange}
             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
           />
-          </div>
-        <button type="submit" className="mt-6 w-full py-3 bg-gray-700 text-white font-bold rounded-lg hover:bg-gray-800 transition-colors">
-          Add Property
-        </button>
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors duration-300"
+          >
+            Add Property
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/components/PropertyListing.js
+++ src/components/PropertyListing.js
@@ -83,6 +83,90 @@
 // };
 
 // export default PropertyListing;
+// import React, { useState, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faLocationDot, faPhone, faBath, faBed, faRulerCombined } from '@fortawesome/free-solid-svg-icons';
+
+// const PropertyListing = () => {
+//   const [properties, setProperties] = useState([]);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const fetchProperties = async () => {
+//       try {
+//         const response = await fetch('https://room-rooster.vercel.app/details');
+//         if (response.ok) {
+//           const data = await response.json();
+//           setProperties(data);
+//         } else {
+//           console.error('Failed to fetch properties');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching properties:', error);
+//       }
+//     };
+
+//     fetchProperties();
+//   }, []);
+
+//   const handleDetailsClick = (id) => {
+//     navigate(`/details/${id}`);
+//   };
+
+//   return (
+//     <div className="container mx-auto py-12">
+//       <div className="text-center mb-12">
+//         <h1 className="text-3xl font-bold text-[#596E79]">Property Listing</h1>
+//         <p className="text-xl font-semibold text-[#596E79] mt-4">
+//           Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod sit diam justo sed rebum.
+//         </p>
+//       </div>
+//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-6">
+//         {properties.map((detail) => (
+//           <div key={detail._id} className="bg-gray-200 rounded-lg shadow-lg overflow-hidden transition-transform transform">
+//             <div className="relative overflow-hidden">
+//               <a href="#">
+//                 <img className="w-full h-48 object-cover hover:scale-105" src={detail.image || "default-image-url.jpg"} alt={detail.name} />
+//               </a>
+//               <div className="absolute bottom-0 left-0 bg-[#596E79] text-[#F0ECE3] text-xs font-semibold uppercase py-1 px-8 rounded-tr-lg">
+//                 {detail.name}
+//               </div>
+//             </div>
+//             <div className="p-4 bg-[#e9e5db]">
+//               <h5 className="text-xl font-bold text-gray-700 mb-3">₹{detail.price} /month</h5>
+//               <p className="text-gray-600 mb-2 flex items-center">
+//                 <FontAwesomeIcon icon={faLocationDot} /> {detail.location}
+//               </p>
+//               <p className="text-gray-600 mb-2 flex items-center">
+//                 <FontAwesomeIcon icon={faPhone} /> {detail.phoneNumber}
+//               </p>
+//               <button
+//                 className="bg-[#596E79] text-[#F0ECE3] py-2 px-4 rounded hover:border-2 hover:border-[#596E79] hover:text-[#596E79] hover:bg-transparent"
+//                 onClick={() => handleDetailsClick(detail._id)}
+//               >
+//                 Details
+//               </button>
+//             </div>
+//             <div className="flex border-t border-dashed border-gray-700 bg-[#e9e5db]">
+//               <small className="flex-1 text-center border-r border-dashed border-gray-700 py-2 text-gray-600 font-bold">
+//                 <FontAwesomeIcon icon={faRulerCombined} /> {detail.sqft} Sqft
+//               </small>
+//               <small className="flex-1 text-center border-r border-dashed border-gray-700 py-2 text-gray-600 font-bold">
+//                 <FontAwesomeIcon icon={faBed} /> {detail.bed} Bed
+//               </small>
+//               <small className="flex-1 text-center py-2 text-gray-600 font-bold">
+//                 <FontAwesomeIcon icon={faBath} /> {detail.bath} Bath
+//               </small>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PropertyListing;
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
