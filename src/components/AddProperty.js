11e6f7f1f616605af94a6b812db4478b625fa8b9
Divy-Shah19 <divy.shah@bsptechno.com>
2024-08-14 14:29:08 +0530

    dd

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -83,7 +83,7 @@ const AddProperty = ({ onAddProperty }) => {
     console.log('FormData:', formData); // Add this line to check the data
   
     try {
-      const response = await fetch("https://room-rooster.vercel.app/details", {
+      const response = await fetch("https://room-rooster.vercel.app/dd", {
         method: "POST",
         body: formData,
       });
