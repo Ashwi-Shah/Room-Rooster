9c247b102f1ece9c96b930fe6396dd5b707eab98
DIGIART <ashwishah1808@gmail.com>
2024-11-14 04:30:08 +0530

    design changes

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -45,7 +45,7 @@ const ResultsPage = () => {
 
   return (
     <div className="container mx-auto py-12">
-      <div className="text-center mb-12 mt-12">
+      <div className="text-center mb-12">
         <h1 className="text-3xl font-bold text-[#596E79]">Property Listing</h1>
         <p className="text-xl font-semibold text-[#596E79] mt-4">
         Find the perfect rental home with our diverse selection of properties tailored to your needs
